383333a00fea8a370021a5182d14972abc2c13cc
zhangchuang <zhangchuang0013@gmail.com>
2021-11-01 23:24:20 +0800

    添加单调栈模块

---
+++ 单调栈/src/LC84_柱状图中最大的矩形.java
@@ -0,0 +1,105 @@
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Created by zhang on 2021/11/1  22:58
+ */
+public class LC84_柱状图中最大的矩形 {
+
+
+
+    @Test
+    public void test(){
+        int area = largestRectangleArea2(new int[]{2, 1, 5, 6, 2, 3});
+        System.out.println(area);
+    }
+
+
+    // 方法二： 加入前后哨兵节点：
+    public int largestRectangleArea2(int[] heights) {
+        int len = heights.length;
+        if (len == 0) {
+            return 0;
+        }
+
+        if (len == 1) {
+            return heights[0];
+        }
+
+        int res = 0;
+
+        int[] newHeights = new int[len + 2];
+        newHeights[0] = 0;
+        System.arraycopy(heights, 0, newHeights, 1, len);
+        newHeights[len + 1] = 0;
+        len += 2;
+        heights = newHeights;
+
+        Deque<Integer> stack = new ArrayDeque<>(len);
+        // 先放入哨兵，在循环里就不用做非空判断
+        stack.addLast(0);
+
+        for (int i = 1; i < len; i++) {
+            while (heights[i] < heights[stack.peekLast()]) {
+                int curHeight = heights[stack.pollLast()];
+                int curWidth = i - stack.peekLast() - 1;
+                res = Math.max(res, curHeight * curWidth);
+            }
+            stack.addLast(i);
+        }
+        return res;
+    }
+
+
+    // 方法一： 当第一次遍历结束后，如果栈内还存留数据时，需要再次遍历
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        if (len == 0) {
+            return 0;
+        }
+        if (len == 1) {
+            return heights[0];
+        }
+
+        int res = 0;
+        Deque<Integer> stack = new ArrayDeque<>(len);
+        for (int i = 0; i < len; i++) {
+            // 这个 while 很关键，因为有可能不止一个柱形的最大宽度可以被计算出来
+            while (!stack.isEmpty() && heights[i] < heights[stack.peekLast()]) {
+                int curHeight = heights[stack.pollLast()];
+                while (!stack.isEmpty() && heights[stack.peekLast()] == curHeight) {
+                    stack.pollLast();
+                }
+
+                int curWidth;
+                if (stack.isEmpty()) {
+                    curWidth = i;
+                } else {
+                    curWidth = i - stack.peekLast() - 1;
+                }
+
+                // System.out.println("curIndex = " + curIndex + " " + curHeight * curWidth);
+                res = Math.max(res, curHeight * curWidth);
+            }
+            stack.addLast(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int curHeight = heights[stack.pollLast()];
+            while (!stack.isEmpty() && heights[stack.peekLast()] == curHeight) {
+                stack.pollLast();
+            }
+            int curWidth;
+            if (stack.isEmpty()) {
+                curWidth = len;
+            } else {
+                curWidth = len - stack.peekLast() - 1;
+            }
+            res = Math.max(res, curHeight * curWidth);
+        }
+        return res;
+    }
+
+}
